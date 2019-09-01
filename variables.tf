b5f7740b6e4260f743efe0c8f75bf98c1c7d9008
Sachin Kundu <sakundu@microsoft.com>
2019-09-01 09:45:48 +0300

    Add TF sripts for AKS

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "client_id" {}
+variable "client_secret" {}
+
+variable "agent_count" {
+    default = 3
+}
+
+variable "ssh_public_key" {
+    default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+    default = "sakunduk8s"
+}
+
+variable cluster_name {
+    default = "k8s"
+}
+
+variable resource_group_name {
+    default = "k8s"
+}
+
+variable location {
+    default = "West Europe"
+}
+
+variable log_analytics_workspace_name {
+    default = "k8sLogAnalyticsWorkspaceName"
+}
+
+# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
+variable log_analytics_workspace_location {
+    default = "westeurope"
+}
+
+# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
+variable log_analytics_workspace_sku {
+    default = "PerGB2018"
+}
