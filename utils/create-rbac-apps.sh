f1d69478ab17c4c6ae0c79f6ba6cacb1a7732b20
msebragge <ebragge@microsoft.com>
2021-02-15 12:04:35 +0200

    Verify that admin consent is set correctly

--- utils/create-rbac-apps.sh
+++ utils/create-rbac-apps.sh
@@ -5,7 +5,7 @@ aksname=$1
 # Create the Azure AD application
 serverApplicationId=`az ad app create --display-name "${aksname}Server" --identifier-uris "https://${aksname}Server" --query appId -o tsv`
 
-# Update the application group memebership claims
+# Update the application group membership claims
 az ad app update --id $serverApplicationId --set groupMembershipClaims=All
 
 # Create a service principal for the Azure AD application
@@ -17,7 +17,21 @@ serverApplicationSecret=`az ad sp credential reset --name $serverApplicationId -
 az ad app permission add --id $serverApplicationId --api 00000003-0000-0000-c000-000000000000 --api-permissions e1fe6dd8-ba31-4d61-89e7-88639da4683d=Scope 06da0dbc-49e2-44d2-8312-53f166ab848a=Scope 7ab1d382-f21e-4acd-a863-ba3e13f7da61=Role
 
 az ad app permission grant --id $serverApplicationId --api 00000003-0000-0000-c000-000000000000
-az ad app permission admin-consent --id  $serverApplicationId
+
+admin_consent=0
+while [ $admin_consent -eq 0 ]
+do
+    az ad app permission admin-consent --id $serverApplicationId
+
+    if [[ ${?} -eq 0 ]]
+    then
+        echo "Admin consent ok"
+        admin_consent=1
+    else
+        echo "Retry after 5 seconds"
+        sleep 5
+    fi
+done < <(find . -type f)
 
 clientApplicationId=`az ad app create --display-name "${aksname}Client" --native-app --reply-urls "https://${aksname}Client" --query appId -o tsv`
 az ad sp create --id $clientApplicationId
